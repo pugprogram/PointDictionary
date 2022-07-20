071218a7942eb9bf1a10fe537b00c7a7b66c5341
pugprogram <108753375+pugprogram@users.noreply.github.com>
2022-07-20 22:07:39 +0300

    Update PointDictionary.cpp

--- PointDictionary.cpp
+++ PointDictionary.cpp
@@ -37,97 +37,6 @@ namespace DB
         calculateBytesAllocated();
     }
 
-
-
-    void IPolygonDictionary::convertKeyColumns(Columns& key_columns, DataTypes& key_types) const
-    {
-        if (key_columns.size() != 2)
-            throw Exception(ErrorCodes::TYPE_MISMATCH,
-                "Dictionary {} key lookup structure does not match, expected two columns of coordinates with type Float64",
-                getFullName());
-        auto float_64_type = std::make_shared<DataTypeFloat64>();
-        size_t key_types_size = key_types.size();
-        for (size_t key_type_index = 0; key_type_index < key_types_size; ++key_type_index)
-        {
-            auto& key_type = key_types[key_type_index];
-            if (float_64_type->equals(*key_type))
-                continue;
-            auto& key_column_to_cast = key_columns[key_type_index];
-            ColumnWithTypeAndName column_to_cast = { key_column_to_cast, key_type, "" };
-            auto casted_column = castColumnAccurate(column_to_cast, float_64_type);
-            key_column_to_cast = std::move(casted_column);
-            key_type = float_64_type;
-        }
-    }
-
-
-    ColumnPtr IPolygonDictionary::getColumn(
-        const std::string& attribute_name,
-        const DataTypePtr& result_type,
-        const Columns& key_columns,
-        const DataTypes&,
-        const ColumnPtr& default_values_column) const
-    {
-        const auto requested_key_polygons = extractPolygons(key_columns);
-        const auto& attribute = dict_struct.getAttribute(attribute_name, result_type);
-        DefaultValueProvider default_value_provider(attribute.null_value, default_values_column);
-        size_t attribute_index = dict_struct.attribute_name_to_index.find(attribute_name)->second;
-        const auto& attribute_values_column = attributes_columns[attribute_index];
-        auto result = attribute_values_column->cloneEmpty();
-        result->reserve(requested_key_points.size());
-        if (unlikely(attribute.is_nullable))
-        {
-            getItemsImpl<Field>(
-                requested_key_polygons,
-                [&](size_t row) { return (*attribute_values_column)[row]; },
-                [&](Field& value) { result->insert(value); },
-                default_value_provider);
-        }
-        else
-        {
-            auto type_call = [&](const auto& dictionary_attribute_type)
-            {
-                using Type = std::decay_t<decltype(dictionary_attribute_type)>;
-                using AttributeType = typename Type::AttributeType;
-                using ValueType = DictionaryValueType<AttributeType>;
-                using ColumnProvider = DictionaryAttributeColumnProvider<AttributeType>;
-                using ColumnType = typename ColumnProvider::ColumnType;
-                const auto attribute_values_column_typed = typeid_cast<const ColumnType*>(attribute_values_column.get());
-                if (!attribute_values_column_typed)
-                    throw Exception(ErrorCodes::BAD_ARGUMENTS, "An attribute type should be same as dictionary type");
-                ColumnType& result_column_typed = static_cast<ColumnType&>(*result);
-                if constexpr (std::is_same_v<ValueType, Array>)
-                {
-                    getItemsImpl<ValueType>(
-                        requested_key_polygons,
-                        [&](size_t row) { return (*attribute_values_column)[row].get<Array>(); },
-                        [&](Array& value) { result_column_typed.insert(value); },
-                        default_value_provider);
-                }
-                else if constexpr (std::is_same_v<ValueType, StringRef>)
-                {
-                    getItemsImpl<ValueType>(
-                        requested_key_points,
-                        [&](size_t row) { return attribute_values_column->getDataAt(row); },
-                        [&](StringRef value) { result_column_typed.insertData(value.data, value.size); },
-                        default_value_provider);
-                }
-                else
-                {
-                    auto& attribute_data = attribute_values_column_typed->getData();
-                    auto& result_data = result_column_typed.getData();
-                    getItemsImpl<ValueType>(
-                        requested_key_points,
-                        [&](size_t row) { return attribute_data[row]; },
-                        [&](auto value) { result_data.emplace_back(value); },
-                        default_value_provider);
-                }
-            };
-            callOnDictionaryAttributeType(attribute.underlying_type, type_call);
-        }
-        return result;
-    }
-
     Pipe IPointDictionary::read(const Names& column_names, size_t, size_t) const
     {
         if (!configuration.store_point_key_column)
@@ -158,6 +67,7 @@ namespace DB
         auto source = std::make_shared<SourceFromSingleChunk>(Block(result_columns));
         return Pipe(std::move(source));
     }
+    
 
     void IPointDictionary::setup()
     {
@@ -207,7 +117,7 @@ namespace DB
             blockToAttributes(block);
     }
 
-    void IPolygonDictionary::calculateBytesAllocated()
+    void IPointDictionary::calculateBytesAllocated()
     {
         if (configuration.store_point_key_column)
             bytes_allocated += key_attribute_column->allocatedBytes();
@@ -216,32 +126,7 @@ namespace DB
         bytes_allocated = points.size() * 2 * sizeof(Point);
     }
 
-    std::vector<IPolygonDictionary::Point> IPolygonDictionary::extractPoints(const Columns& key_columns)
-    {
-        if (key_columns.size() != 2)
-            throw Exception(ErrorCodes::BAD_ARGUMENTS, "Expected two columns of coordinates with type Float64");
-        const auto* column_x = typeid_cast<const ColumnVector<Float64>*>(key_columns[0].get());
-        const auto* column_y = typeid_cast<const ColumnVector<Float64>*>(key_columns[1].get());
-        if (!column_x || !column_y)
-            throw Exception(ErrorCodes::TYPE_MISMATCH, "Expected columns of Float64");
-        const auto rows = key_columns.front()->size();
-        std::vector<Point> result;
-        result.reserve(rows);
-        for (size_t row = 0; row < rows; ++row)
-        {
-            auto x = column_x->getElement(row);
-            auto y = column_y->getElement(row);
-            if (isNaN(x) || isNaN(y))
-                throw Exception(ErrorCodes::BAD_ARGUMENTS,
-                    "PolygonDictionary input point component must not be NaN");
-            if (std::isinf(x) || std::isinf(y))
-                throw Exception(ErrorCodes::BAD_ARGUMENTS,
-                    "PolygonDictionary input point component must not be infinite");
-            result.emplace_back(x, y);
-        }
-        return result;
-    }
-
+   
     ColumnUInt8::Ptr IPointDictionary::hasKeys(const Columns& key_columns, const DataTypes&) const
     {
         std::vector<IPolygonDictionary::Polygon> polygons = extractPolygons(key_columns);
@@ -326,7 +211,7 @@ namespace DB
             }
             void AddPoint(IPointDictionary::Coord x, IPointDictionary::Coord y) {
                 auto& lastpoint = dest.back();
-                laspoint.emplace_back(&x, &y);
+                lastpoint.emplace_back(&x, &y);
             }
         };
 
